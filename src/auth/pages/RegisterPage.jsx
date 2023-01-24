d62b0fc70a096f56b491427cbba701e45267c78e
lu-perez <luperez@teco.com.ar>
2023-01-24 15:09:41 -0300

    authLayout + LoginPage + RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import AuthLayout from '../layout/AuthLayout'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Name" type="text" placeholder="John Doe" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Email" type="email" placeholder="email@example.com" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Password" type="password" placeholder="password" fullWidth />
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
 
